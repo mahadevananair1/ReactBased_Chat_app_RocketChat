948a05c8f473078440bf789f7d4a5c4fc1a83b33
mahadevananair1 <mahadevananair@gmail.com>
2021-05-22 10:04:42 +0530

    rocket app starter template

---
+++ rocketchat/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  // your config
+
+})
+
+const auth = firebase.auth();
+const firestrore = firebase.firestore();
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
