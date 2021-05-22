6eeedbc83fe4c2c276960fb2d275e7aa1eb25111
mahadevananair1 <mahadevananair@gmail.com>
2021-05-22 14:19:50 +0530

    The Chat works yay

--- rocketchat/src/App.js
+++ rocketchat/src/App.js
@@ -1,47 +1,47 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
 
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-
+import 'firebase/analytics';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  // your config
-    apiKey: "AIzaSyD80I_S3W66ulRZ0wfNf77zVywrID5KMbI",
-    authDomain: "rocketchat-21ac4.firebaseapp.com",
-    projectId: "rocketchat-21ac4",
-    storageBucket: "rocketchat-21ac4.appspot.com",
-    messagingSenderId: "557684864916",
-    appId: "1:557684864916:web:1c3c403b1b9c3c8a814181",
-    measurementId: "G-P3TYLFKHQX"
-
-})
+  apiKey: "AIzaSyDPxMfHTkLQnN7JbNHLWlu76-sdaIW-g5Q",
+    authDomain: "rocketleague-f4ebf.firebaseapp.com",
+    projectId: "rocketleague-f4ebf",
+    storageBucket: "rocketleague-f4ebf.appspot.com",
+    messagingSenderId: "780372147818",
+    appId: "1:780372147818:web:170fe979182142a0ec5413",
+    measurementId: "G-598J8BQ5PC"
+});
 
 const auth = firebase.auth();
-const firestrore = firebase.firestore();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
 
-
-const [user] = useAuthState(auth);
 
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
       </header>
 
       <section>
-        {user ? <ChatRoom/> : <SignIn />}
+        {user ? <ChatRoom /> : <SignIn />}
       </section>
 
     </div>
   );
 }
-
 
 function SignIn() {
 
@@ -50,16 +50,75 @@ function SignIn() {
     auth.signInWithPopup(provider);
   }
 
-  return(
-    <button onClick = {signInWithGoogle } >Sign in with Gooogle</button>
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
   )
+
 }
 
 function SignOut() {
   return auth.currentUser && (
-
-    <button onClick = {() => auth.signOut()}>Sign Out</button>
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
   )
+}
+
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(messagesRef, { idField: 'id' });
+
+  const [formValue,setFormValue] = useState('');
+  
+  const sendMessage = async(e) => {
+
+    e.preventDefault();
+
+    const {uid} = auth.currentUser;
+
+    await messagesRef.add({
+      text:formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid
+    });
+
+    setFormValue('');
+  }
+
+  
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <form onSubmit = {sendMessage}>
+          <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+          <button type="submit">submit</button>
+      </form>
+
+    </main>
+
+    
+  </>)
+}
+
+
+function ChatMessage(props) {
+  const {text,uid} = props.message;
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  console.log(props)
+  return (
+    <div>
+      <p>{text}</p>
+    </div>
+  )
+  
 }
 
 
